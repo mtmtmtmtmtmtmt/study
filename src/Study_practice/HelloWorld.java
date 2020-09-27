83202729179848500d5191e66348747b8a8a6521
MT <905018962@qq.com>
2020-09-27 15:50:04 +0800

    第二次修改，添加第一行注释（test_01）

--- src/Study_practice/HelloWorld.java
+++ src/Study_practice/HelloWorld.java
@@ -8,6 +8,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello,world!");
 
-        //测试回退操作与查看master与origin_master的指向（test_01）
+        //测试回退操作与查看master与origin_master的指向（test_02）
     }
 }
