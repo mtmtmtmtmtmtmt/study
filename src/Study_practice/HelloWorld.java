eaa4ebec565ac54e3f27a73be77bb37225e8f2bf
MT <905018962@qq.com>
2020-09-27 15:37:49 +0800

    第一次修改，添加println("HelloWorld");

--- src/Study_practice/HelloWorld.java
+++ src/Study_practice/HelloWorld.java
@@ -2,7 +2,10 @@ package Study_practice;
 
 /**
  * @author MT
- * @create 2020-09-27-14:25
+ * @create 2020-09-27-15:31
  */
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello,world!");
+    }
 }
